ece6b6dd983dc0c8d1ff6712759e39d44dd1d597
Gaurav Raj <gauravraj0811@gmail.com>
2024-11-12 23:08:10 +0530

    update

--- src/ninNin.java
+++ src/ninNin.java
@@ -26,6 +26,53 @@ public class ninNin {
         }
         return dp[day][last]=max;
     }
+
+    //tabulation
+
+    public static int find2(int[][] points,int n){
+        int[][] dp=new int[n][4];
+
+        dp[0][0]=Math.max(points[0][1],points[0][2]);
+        dp[0][1]=Math.max(points[0][0],points[0][2]);
+        dp[0][2]=Math.max(points[0][0],points[0][1]);
+        dp[0][3]=Math.max(points[0][0],Math.max(points[0][1],points[0][2]));
+
+        for(int day=1;day<n;day++){
+            for(int last=0;last<4;last++){
+                dp[day][last]=0;
+                for(int task=0;task<=2;task++){
+                    int activity=points[day][task]+dp[day-1][task];
+                    dp[day][last]=Math.max(dp[day][last],activity);
+                }
+            }
+        }
+        return dp[n-1][3];
+    }
+
+    public static int find3(int[][] points,int n){
+        int[] prev=new int[4];
+
+        prev[0]=Math.max(points[0][1],points[0][2]);
+        prev[1]=Math.max(points[0][0],points[0][2]);
+        prev[2]=Math.max(points[0][0],points[0][1]);
+        prev[3]=Math.max(points[0][0],Math.max(points[0][1],points[0][2]));
+
+        for(int day=1;day<n;day++){
+            int[] temp=new int[4];
+            for(int last=0;last<4;last++){
+                temp[last]=0;
+                for(int task=0;task<=2;task++){
+                    if(task!=last)
+                        temp[last]=Math.max(temp[last],points[day][task]+prev[task]);
+                }
+            }
+            prev=temp;
+        }
+        return prev[3];
+    }
+
+
+
     public static void main(String[] args) {
         int[][] points = {{10, 40, 70},
                 {20, 50, 80},
@@ -33,5 +80,7 @@ public class ninNin {
 
         int n = points.length;
         System.out.println(maximumPoints( points, n));
+        System.out.println(find2(points,n));
+        System.out.println(find3(points,n));
     }
 }
